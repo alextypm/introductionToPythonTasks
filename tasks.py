640425911e8f08468cb0036c0e9b716d1b9dcd1b
AlexTyPM <alextypm1@gmail.com>
2024-12-28 14:16:48 -0500

    Question 10

--- tasks.py
+++ tasks.py
@@ -91,3 +91,19 @@ except IndexError:
     print("Error")
 -------------------------------------------------------------------------------
 '''
+'''
+-------------------------------------------------------------------------------
+# THIS IS THE RESULT OF QUESTION 10 #
+stocks = {'TSLA': 912.86 , 'BBBY': 24.84, 'AAPL': 174.26, 'SOFI': 6.92, 'KIRK': 8.72,
+          'AURA': 22.12, 'AMZN': 141.28, 'EMBK': 12.29, 'LVLU': 2.33}
+
+num_stocks = int(input())
+total_cost = 0.0
+for i in range(num_stocks):
+    stock = input().strip()
+    total_cost += stocks[stock]
+
+print(f"Total price: ${total_cost:.2f}")
+-------------------------------------------------------------------------------
+
+'''
